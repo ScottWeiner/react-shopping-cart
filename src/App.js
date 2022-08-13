94f73eddc03a75fe98f0e8cb3acc80d07da8ef77
Scott W <scottaza@gmail.com>
2022-08-13 11:22:34 -0700

    Created functons to manage orders in a Admin page

--- src/App.js
+++ src/App.js
@@ -1,35 +1,34 @@
 //feature 1 - example
 import React, { Component } from 'react'
+import { BrowserRouter, Route, Link, Routes } from 'react-router-dom'
 
-import Products from './components/Products'
-import Filter from './components/Filter'
-import Cart from './components/Cart'
 import store from './store/store'
 import { Provider } from 'react-redux'
+import AdminScreen from './screens/AdminScreen';
+import HomeScreen from './screens/HomeScreen';
 
 class App extends Component {
 
 
   render() {
     return (
+
       <Provider store={store}>
-        <div className="grid-container">
-          <header>
-            <a href="/">React Shopping Cart</a>
-          </header>
-          <main>
-            <div className="content">
-              <div className="main">
-                <Filter />
-                <Products />
-              </div>
-              <div className="sidebar">
-                <Cart />
-              </div>
-            </div>
-          </main>
-          <footer>Some rights reserved, but not all.</footer>
-        </div>
+        <BrowserRouter>
+          <div className="grid-container">
+            <header>
+              <Link to="/">React Shopping Cart</Link>
+              <Link to="/admin">Admin</Link>
+            </header>
+            <main>
+              <Routes>
+                <Route path='/admin' element={<AdminScreen />} />
+                <Route path='/' exact element={<HomeScreen />} />
+              </Routes>
+            </main>
+            <footer>Some rights reserved, but not all.</footer>
+          </div>
+        </BrowserRouter>
       </Provider>
 
     );
